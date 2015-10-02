42c0b55c8217ef5e58235fc29833b1596dfa3577
Michael Adams <madams6216@gmail.com>
2015-10-02 14:09:27 -0400

    added a new file

---
+++ ScrollViews2/CustomScrollViewController.swift
@@ -0,0 +1,105 @@
+//
+//  CustomScrollViewController.swift
+//  ScrollViews2
+//
+//  Created by michael adams on 10/2/15.
+//  Copyright © 2015 michael adams. All rights reserved.
+//
+
+import UIKit
+
+class CustomScrollViewController: UIViewController, UIScrollViewDelegate {
+    
+    
+    var containerView: UIView!
+
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Set up the container view to hold your custom view hierarchy
+        let containerSize = CGSize(width: self.view.frame.size.width, height: 640.0)
+        containerView = UIView(frame: CGRect(origin: CGPoint(x: 0, y: 0), size:containerSize))
+        scrollView.addSubview(containerView)
+        
+        // Set up your custom view hierarchy
+        let redView = UIView(frame: CGRect(x: 0, y: 0, width: 640, height: 80))
+        redView.backgroundColor = UIColor.redColor();
+        containerView.addSubview(redView)
+        
+        let blueView = UIView(frame: CGRect(x: 0, y: 560, width: 640, height: 80))
+        blueView.backgroundColor = UIColor.blueColor();
+        containerView.addSubview(blueView)
+        
+        let greenView = UIView(frame: CGRect(x: 160, y: 160, width: 320, height: 320))
+        greenView.backgroundColor = UIColor.greenColor();
+        containerView.addSubview(greenView)
+        
+        let imageView = UIImageView(frame: self.view.frame)
+        imageView.image = UIImage(named: "slow.png")
+        //imageView.center = CGPoint(x: 320, y: 320);
+        imageView.contentMode = UIViewContentMode.ScaleToFill
+        containerView.addSubview(imageView)
+        
+        // Tell the scroll view the size of the contents
+        scrollView.contentSize = containerSize;
+        
+        // Set up the minimum & maximum zoom scales
+        let scrollViewFrame = scrollView.frame
+        let scaleWidth = scrollViewFrame.size.width / scrollView.contentSize.width
+        let scaleHeight = scrollViewFrame.size.height / scrollView.contentSize.height
+        let minScale = min(scaleWidth, scaleHeight)
+        
+        scrollView.minimumZoomScale = minScale
+        scrollView.maximumZoomScale = 1.0
+        scrollView.zoomScale = 1.0
+        
+        centerScrollViewContents()
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func centerScrollViewContents() {
+        let boundsSize = scrollView.bounds.size
+        var contentsFrame = containerView.frame
+        
+        if contentsFrame.size.width < boundsSize.width {
+            contentsFrame.origin.x = (boundsSize.width - contentsFrame.size.width) / 2.0
+        } else {
+            contentsFrame.origin.x = 0.0
+        }
+        
+        if contentsFrame.size.height < boundsSize.height {
+            contentsFrame.origin.y = (boundsSize.height - contentsFrame.size.height) / 2.0
+        } else {
+            contentsFrame.origin.y = 0.0
+        }
+        
+        containerView.frame = contentsFrame
+    }
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return containerView
+    }
+    
+    func scrollViewDidZoom(scrollView: UIScrollView) {
+        centerScrollViewContents()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
